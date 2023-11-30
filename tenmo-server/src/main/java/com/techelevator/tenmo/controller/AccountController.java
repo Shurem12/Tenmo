fa98543dac793457d95d285e5c995f5937052f1c
shurem jama <shurem65@gmail.com>
2023-11-30 09:47:55 -0500

    created transfer table

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -0,0 +1,45 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@PreAuthorize("isAuthenticated")
+public class AccountController {
+    @Autowired
+    private AccountDao accountDao;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/account", method = RequestMethod.POST)
+    public Account createAccount(@RequestParam int userId) {
+        return accountDao.createAccount(userId);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/account", method = RequestMethod.DELETE)
+    public Account deleteAccount(@RequestParam int accountId) {
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/account", method = RequestMethod.GET)
+    public Account listTransfers(@RequestParam int userId) {
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/account/4", method = RequestMethod.POST)
+    public Account sendMoney(@RequestParam int userId,  int senderAccountId, int recipientAccountId, double amount) {
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/account/5", method = RequestMethod.POST)
+    public Account retrieveDetails(@RequestParam int userId) {
+        return null;
+    }
+}
