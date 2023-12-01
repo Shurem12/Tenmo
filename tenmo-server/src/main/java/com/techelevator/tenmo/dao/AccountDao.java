2d10455f1af5eba46e3d2e0cea2253df69cf2d60
Dominic.Lobban <dominic.lobban@gmail.com>
2023-12-01 09:20:43 -0500

    refactor

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -5,15 +5,11 @@ import com.techelevator.tenmo.model.Account;
 import java.util.List;
 
 public interface AccountDao {
+    Account getBalanceById(int accountId);
+
     Account createAccount(int userId);
 
-    boolean deleteAccount(int userID, int  accountId);
+    boolean deleteAccount(int userID, int accountId);
 
-    List<Account> listTransfers(int userId);
-
-    Account sendMoney( int senderAccountId, int recipientAccountId, double amount);
-
-    Account getBalance(int userId);
-
-    Account grabDetails(int userId);
+    Account sendMoney(int senderAccountId, int recipientAccountId, double amount);
 }
