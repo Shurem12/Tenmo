61db4c5164bfe9dce7c576027a37534f66e4d2c9
Dominic.Lobban <dominic.lobban@gmail.com>
2023-12-01 09:19:15 -0500

    added list methods

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public interface TransferDao {
     Transfer getTransferById(int transferId);
+    Transfer addTransfer();
+    Transfer removeTransfer(int transferId);
     List<Transfer> listTransfersByUserId(int userId);
+    List<Transfer> listTransfersBySenderUserId(int senderUserId);
+    List<Transfer> listTransfersBySenderAccountId(int senderAccountId);
     List<Transfer> listTransfersByAccountId(int accountId);
+    List<Transfer> listTransfersByRecipientUserId(int recipientUserId);
+    List<Transfer> listTransfersByRecipientAccountId(int recipientAccountId);
 }
