5933cf4d02e00cd5eb236abb69aa7e00fe9739d0
Dominic.Lobban <dominic.lobban@gmail.com>
2023-12-02 14:41:47 -0500

    updated status enums

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS tenmo_user, account, transfer;
 
-DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id, seq_transfer_id;
 
 -- Sequence to start user_id values at 1001 instead of 1
 CREATE SEQUENCE seq_user_id
@@ -32,15 +32,22 @@ CREATE TABLE account (
 	CONSTRAINT PK_account PRIMARY KEY (account_id),
 	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
-CREATE TYPE status AS ENUM ('PENDING','APPROVED');
+
+-- Sequence to start transfer_id values at 3001 instead of 1
+CREATE SEQUENCE seq_transfer_id
+  INCREMENT BY 1
+  START WITH 3001
+  NO MAXVALUE;
+
+CREATE TYPE status AS ENUM ('CANCELLED','PENDING','APPROVED','REJECTED');
 CREATE table transfer(
-        transfer_id serial primary key,
+        transfer_id int not null default nextval('seq_transfer_id') primary key,
         sender_user_id int not null,
         sender_account_id int not null,
         recipient_user_id int not null,
         recipient_account_id int not null,
-        transfer_timestamp timestamp,
-        amount_sent numeric,
+        transfer_timestamp timestamp not null,
+        amount_sent numeric not null,
         transfer_status status not null,
         foreign key (sender_user_id) references tenmo_user(user_id),
         foreign key (sender_account_id) references account(account_id),
